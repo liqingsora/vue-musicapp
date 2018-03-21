a9d6e6fa3c15871f5fd4fed858b5daf03f072704
liqingsora <544260097@qq.comm>
2018-03-21 16:38:42 +0800

    第三章增加

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		排行页面
+	</div>
+</template>
+
+<script type="text/ecmascript6">
+</script>
+
+<style scoped lang="stylus" type="stylesheet/stylus">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Recommend from 'components/recommend/recommend'
+import Singer from 'components/singer/singer'
+import Rank from 'components/rank/rank'
+import Search from 'components/search/search'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',
+      redirect:'/recommend'//重定向，默认进入推荐页
+    },
+    {
+      path:'/recommend',
+      component:Recommend//推荐
+    },
+    {
+      path:'/singer',
+      component:Singer//歌手
+    },
+    {
+      path:'/rank',
+      component:Rank//排行
+    },
+    {
+      path:'/search',
+      component:Search//搜索
     }
   ]
 })
